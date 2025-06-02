5c6ceb915b7fa4afb2c6ca4c5a2ae94bb3778acd
WalterJoel <walter.valdivia@ucsp.edu.pe>
2025-06-01 22:22:54 -0500

    [feat] Manejando Excepciones en los validadores

--- src/aws-infrastructure/lambda-handlers/createAppointment.ts
+++ src/aws-infrastructure/lambda-handlers/createAppointment.ts
@@ -2,18 +2,28 @@ import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
 let cachedServer;
 
 export const handler = async (event, context) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule);
+
     nestApp.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
         transform: true,
+        exceptionFactory: (validationErrors = []) => {
+          const messages = validationErrors.map(
+            (err) =>
+              `${err.property} has wrong value ${err.value}, ${Object.values(
+                err.constraints,
+              ).join(', ')}`,
+          );
+          return new BadRequestException(messages);
+        },
       }),
     );
 
@@ -24,9 +34,10 @@ export const handler = async (event, context) => {
       .build();
 
     const document = SwaggerModule.createDocument(nestApp, config);
-
     SwaggerModule.setup('api', nestApp, document);
+
     await nestApp.init();
+
     cachedServer = serverlessExpress({
       app: nestApp.getHttpAdapter().getInstance(),
     });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +10,15 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
+      exceptionFactory: (validationErrors = []) => {
+        const messages = validationErrors.map(
+          (err) =>
+            `${err.property} has wrong value ${err.value}, ${Object.values(
+              err.constraints,
+            ).join(', ')}`,
+        );
+        return new BadRequestException(messages);
+      },
     }),
   );
 
